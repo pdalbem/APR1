c7ba63ecb12fa73f19772f3959bdae6209af9160
pablo <dalbem@gmail.com>
2024-06-03 12:05:17 -0300

    Códigos

--- Arquivos/agenda.py
+++ Arquivos/agenda.py
@@ -1,5 +1,29 @@
-from lib.arquivo import *
-from lib.tela import *
+import os
+
+def existe_arquivo(arq):
+	return os.path.exists(arq)
+		
+def criar_arquivo(arq):
+	a=open(arq,'w')
+	a.close()
+
+def limpar_tela():
+     os.system('cls' if os.name == 'nt' else 'clear')
+
+def exibir_cabecalho(texto):
+    print("-" * 30)
+    print(texto.center(30))
+    print("-" * 30)
+
+def exibir_menu():
+    limpar_tela()
+    exibir_cabecalho("MENU")
+
+    itens_menu=["Cadastrar contato", "Listar contatos", "Pesquisar contato"]
+    for i in range(len(itens_menu)):
+         print(f"{i+1} - {itens_menu[i]}")
+    print("0 - Sair") 
+    print("-" * 30)
 
 def gravar_contato(arq,tupla):
 	a=open(arq,'a')
--- Arquivos/lib/arquivo.py
+++
@@ -1,8 +0,0 @@
-import os
-
-def existe_arquivo(arq):
-	return os.path.exists(arq)
-		
-def criar_arquivo(arq):
-	a=open(arq,'w')
-	a.close()
